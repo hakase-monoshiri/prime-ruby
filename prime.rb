fb008f34a55f1940ebd73d97f0d52f104b457cb4
hakase-monoshiri <ebenaeleazer@outlook.com>
2020-12-15 20:35:24 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+    test_numbers = (2...integer).to_a
+    if test_numbers.any? { |test| integer % test == 0}
+        return false
+    elsif integer < 2
+        return false
+    elsif integer == 2 || 3
+        return true
+    else
+        return true
+    end
+end
